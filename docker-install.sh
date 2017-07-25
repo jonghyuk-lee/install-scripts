08faf3fb29b08cc342e9955f3d57d742528c8a4f
JongHyuk Lee <jhj.lee@samsung.com>
2017-07-25 01:21:33 +0000

    add docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yum remove -y docker docker-common docker-selinux docker-engine
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum install -y docker-ce
+
+systemctl enable docker
+systemctl start docker
+
+exit 0
+
